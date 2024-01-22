f138a402ca8247ca981e6e76a5e46dd924df9da9
adibmuzakki <adibmuzakki2021@gmail.com>
2024-01-22 14:59:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dwi_androidbundle/MainActivity2.kt
@@ -0,0 +1,27 @@
+package com.example.dwi_androidbundle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var namaView: TextView
+    private lateinit var kelasView: TextView
+    private lateinit var NISView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        namaView = findViewById(R.id.namaView)
+        kelasView = findViewById(R.id.kelasView)
+        NISView = findViewById(R.id.NISView)
+
+        val extras = intent.extras
+        namaView.text = extras?.getString("nama")
+        kelasView.text = extras?.getString("kelas")
+        NISView.text = extras?.getString("NIS")
+
+    }
+    }
